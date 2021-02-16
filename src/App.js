23662a8f79ddc1fece17744e89b6184e3beb8887
Jessica Dao <xjessicadao@gmail.com>
2021-02-16 13:25:03 -0800

    navtabs fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import About from "./components/pages/About";
+import Discover from "./components/pages/Discover";
+import Search from "./components/pages/Search";
 
 function App() {
   return (
+<Router>
+  <div>
+    <NavTabs />
+    <Route exact path="/" component={About} />
+    <Route exact path="/about" component={Discover} />
+    <Route exact path="/blog" component={Search} />
 
+  </div>
+</Router>
   );
 }
 
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -4,21 +4,27 @@ import "./style.css";
 
 function NavTabs(props) {
   return (
-    <ul className="nav nav-tabs">
+<ul className="nav nav-tabs">
       <li className="nav-item">
-        <a href="#about" onClick={() => props.handlePageChange("About")} className="nav-link">
+        <Link to="/" className={location.pathname === "About" ? "nav-link active" : "nav-link"}>
           About
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a href="#discover" onClick={() => props.handlePageChange("Discover")} className="nav-link">
-          Blog
-        </a>
+        <Link
+          to="/about"
+          className={location.pathname === "/Discover" ? "nav-link active" : "nav-link"}
+        >
+          Discover
+        </Link>
       </li>
       <li className="nav-item">
-        <a href="#Search" onClick={() => props.handlePageChange("Search")} className="nav-link">
-          Contact
-        </a>
+        <Link
+          to="/blog"
+          className={location.pathname === "/Search" ? "nav-link active" : "nav-link"}
+        >
+          Search
+        </Link>
       </li>
     </ul>
   );
